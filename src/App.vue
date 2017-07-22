a569e541585ed5e1a7ef1f05b7635ecf52cad186
Sharad Shinde <shindesharad71@gmail.com>
2017-07-23 00:39:21 +0530

    delete quote is added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="container">
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <quote-grid :quotes="quotes"></quote-grid>
+        <quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quote-grid>
+        <div class="col-sm-12 text-center">
+            <div class="alert alert-info">info: Click on Quote to Delete it.</div>
+        </div>
     </div>
 </template>
 
@@ -23,6 +26,9 @@
         methods: {
             newQuote(quote) {
                 this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
             }
         }
     }
