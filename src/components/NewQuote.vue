a5a845ea0d91716d045d62f75f09741bc9c8b4a9
Sharad Shinde <shindesharad71@gmail.com>
2017-07-23 00:09:48 +0530

    add new quote textbox added

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-md-offset-3 col-md-6 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-md-offset-3 col-md-6 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+
+export default {
+    data: function() {
+        return {
+            quote: ''
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
