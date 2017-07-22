470466d95e90bba504bd02a04709abe8b3f18865
Sharad Shinde <shindesharad71@gmail.com>
2017-07-22 23:41:39 +0530

    quote grids and methods added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="container">
-    
+        <quote-grid></quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
         data () {
             return {
               maxQuotes: 10,
-              quotes: []  
+              quotes: ['Hey!']  
             }
+        },
+        components: {
+            'quote-grid': QuoteGrid
         }
     }
 </script>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row">
+        <p v-for="quote in quotes">{{quote}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['quotes']
+}
+</script>
+
+<style scoped>
+
+</style>
+
